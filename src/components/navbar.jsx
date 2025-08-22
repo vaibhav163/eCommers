c115a20aaa12444b56db7f3d77813c3d6c058a2b
vaibhav <vaibhavsinghrajput456@gmai.com>
2025-08-22 12:07:14 +0530

    commitit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -57,6 +57,7 @@ const Navbar = () => {
               <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
             </svg>
             </button>
+            <p></p>
         </div>
         </div>
 
